2d7a36fbd673520e7f5fad2432e7f942dc21586d
Jun7892 <sh98022000@gmail.com>
2022-03-09 11:17:51 +0900

    base 적용 / bgm 오류 미해결

--- static/js/second.js
+++ static/js/second.js
@@ -11,3 +11,23 @@ function modalOff() {
     console.log(modal)
     modal.style.display = "none"
 }
+
+let isPlaying = false;
+let audio = document.getElementById("myAudio");
+
+function charm(sound1) {
+
+    // let audio = new Audio(sound1);
+    audio.loop = true;
+    audio.volume = 0.5;
+    isPlaying ? audio.pause() : audio.play();
+
+
+    audio.onplaying = function () {
+        isPlaying = true;
+    };
+    audio.onpause = function () {
+        isPlaying = false;
+    };
+    console.log('aaaa');
+}
