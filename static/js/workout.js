8c99d4b801261b4b1d9fdc626d20368636ce65a3
Jun7892 <sh98022000@gmail.com>
2022-03-10 08:32:12 +0900

    workout page 첫번째 버튼, 모달창 완성

---
+++ static/js/workout.js
@@ -0,0 +1,14 @@
+// 버튼 클릭시 display: flex 값 / 닫기 버튼 클릭시 modal display none 값
+
+const modal = document.getElementById("modal")
+
+function modalOn() {
+    const modal = document.getElementById("modal")
+    modal.style.display = "flex"
+}
+
+function modalOff() {
+    const modal = document.getElementById("modal")
+    console.log(modal)
+    modal.style.display = "none"
+}
