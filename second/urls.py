015601f4cc802078b8f584490e6f7d524b5c8b3c
gaeul <e0540@hanmail.net>
2022-03-14 14:59:04 +0900

    회원가입, 로그인 경로설정

--- second/urls.py
+++ second/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.second_view, name='second'),
+    path('', views.showlogin, name='login') #나중에 로그아웃 후 로그인페이지로 돌아올때 대비
+    path('/sign_up', views.sign_up, name='sign_up'),
+    path('/sign_in', views.sign_in, name='sign_in'),
 ]
