227c4f24a08dbee3081217b39a39f17a824c7fcb
gaeul <e0540@hanmail.net>
2022-03-14 18:22:18 +0900

    회원가입 예외처리 작업중

--- second/views.py
+++ second/views.py
@@ -1,34 +1,42 @@
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_exempt
 
 from second.models import User
 from django.contrib import auth
 
+from second.services.join_service import create_user, check_blank
+
 
 def showloginpage(request):
     if request.method == 'GET': #로그아웃 같은상황으로 이쪽으로 던저주게 되면
         return render(request, 'second.html') #second.html 렌더링해줌.
 
+@csrf_exempt
 def sign_up(request):
     if request.method == 'POST':
-        founduser= User.objects.filter(username=request.POST['username'])
-        if len(founduser) > 0: #같은아이디가 있을때.
-            return render(request, 'second.html', {'error':'해당아이디는 이미 존재합니다.'}) #이거 표시할 곳 필요할듯
-        else:
-            print(request.POST)
-            new_user = User.objects.create(#authmodel에서 id는 pk값으로 쓰기때문에 username을 아이디 저장하는 곳으로 사용.
-                                            username=request.POST['username'],
-                                            password=str(request.POST['password']),
-                                            nickname= request.POST['nickname'],
-                                            gender= request.POST['gender'],
-                                            level= request.POST['level']
-                        ) # 유저생성하고 바로 로그인시켜버렷
-            auth.login(request, new_user)
-            return redirect('main')
-    #만약 get요청으로 들어오면 해당url은 모달창과 연결할 수 있나?
+        username = request.POST['username']
+        password = str(request.POST['password'])
+        nickname = request.POST['nickname']
+        gender = request.POST.get('gender') #체크박스를 아무것도 선택하지 않았을때 값이없기때문에 에러가남.
+        level = request.POST.get('level') # request.POST.get('키값')으로 받아오면 none값이 뜸
+        print(gender, level)
 
-@csrf_exempt
+        msg = check_blank(username,password,nickname,gender,level) #빈칸확인함수
+        if msg != '통과':
+            print(msg)
+            return JsonResponse({'error': msg})
+        else:#빈칸이 아니면
+            founduser= User.objects.filter(username=username)
+            if len(founduser) > 0: #같은아이디가 있을때.
+                return JsonResponse({'error': '해당아이디는 이미 존재합니다.'}) #이거 표시할 곳 필요
+            else: #중복아이디가 아니면
+                result = create_user(username,password,nickname,gender,level)
+                auth.login(request, result) #ajax가 응답을 기다릴까봐 걱정.....
+                return redirect('main')
+
+
 def sign_in(request):
     #장고의 자격증명을 통과하면 founduser생성되고 통과하지 못하면 None반환
     founduser = auth.authenticate(request,
