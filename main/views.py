75b0c395b5947fdb09ebd726a287e2405172ddf8
gaeul <e0540@hanmail.net>
2022-03-15 18:32:21 +0900

    login_required 주석처리

--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-@login_required(login_url='sign_in')
+# @login_required(login_url='sign_in')
 def main_view(request):
     return render(request, 'main.html')
