ffec33a4998d3a4e8cb892e0d211b3313667b182
gaeul <e0540@hanmail.net>
2022-03-15 17:37:32 +0900

    plan에 접속시 유저에 맞게 to_do불러올수 있게 작업중

--- plan/urls.py
+++ plan/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.plan_view, name='plan'),
+    path('<int:id>', views.plan_view, name='plan'),
+    path('', views.plan_view, name='plan')
 ]
