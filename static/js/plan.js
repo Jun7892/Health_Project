83ab0bf7df64f3b4c059f0c3b4b847e2596f031a
Yangs1s <sungjinahim@gmail.com>
2022-03-15 21:54:24 +0900

    meal

--- static/js/plan.js
+++ static/js/plan.js
@@ -272,20 +272,49 @@ window.onload = function () {
         localStorage.setItem(currentDate, JSON.stringify(DATA[currentDate]));
     }
 
-    let coll = document.getElementsByClassName("fa-solid");
-    let i;
+    function meal() {
+        let coll = document.getElementsByClassName("collapsible");
+        let i;
 
-    for (i = 0; i < coll.length; i++) {
-        coll[i].addEventListener("click", function () {
-            this.classList.toggle("active");
-            let content = this.nextElementSibling;
-            if (content.style.display === "block") {
-                content.style.display = "none";
-            } else {
-                content.style.display = "block";
-            }
-        });
+        for (i = 0; i < coll.length; i++) {
+            coll[i].addEventListener("click", function () {
+                this.classList.toggle("active");
+                let content = this.nextElementSibling;
+
+                if (content.style.display === "block") {
+                    content.style.display = "none";
+                } else {
+                    content.style.display = "block";
+                }
+
+
+            });
+        }
     }
+
+    meal();
+
+    // function changeBorderRadius() {
+    //     let coll = document.getElementsByClassName("collapsible");
+    //     let i;
+    //
+    //     for (i = 0; i < coll.length; i++) {
+    //         coll[i].addEventListener("click", function () {
+    //             this.classList.toggle("active");
+    //             let content = this.nextElementSibling;
+    //             if (content.style.display === "block") {
+    //
+    //             } else {
+    //                 coll.style.borderBottomLeftRadius = "unset";
+    //                 coll.style.borderBottomRightRadius = "unset";
+    //             }
+    //
+    //
+    //         });
+    //     }
+    //
+    // }
+    // changeBorderRadius();
 }
 
 
