971102bca15627b7fb9feadfbd34947a8e48bba4
Jun7892 <sh98022000@gmail.com>
2022-03-15 18:34:38 +0900

    제발

--- second/urls.py
+++ second/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.showloginpage, name='showloginpage'), #나중에 로그아웃 후 로그인페이지로 돌아올때 대비
-    path('sign_up', views.sign_up, name='sign_up'),
-    path('sign_in', views.sign_in, name='sign_in'),
-    path('logout', views.logout, name='logout'),
+    path('sign_up/', views.sign_up, name='sign_up'),
+    path('sign_in/', views.sign_in, name='sign_in'),
+    path('logout/', views.logout, name='logout'),
 ]
--- second/views.py
+++ second/views.py
@@ -7,34 +7,43 @@ from second.models import User
 from django.contrib import auth
 
 from second.services.join_service import create_user, check_blank
-
+import json
 
 def showloginpage(request):
-    if request.method == 'GET': #로그아웃 같은상황으로 이쪽으로 던저주게 되면
-        return render(request, 'second.html') #second.html 렌더링해줌.
+    # if request.method == 'GET': #로그아웃 같은상황으로 이쪽으로 던저주게 되면
+    return render(request, 'second.html') #second.html 렌더링해줌.
 
 @csrf_exempt
 def sign_up(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = str(request.POST['password'])
-        nickname = request.POST['nickname']
-        gender = request.POST.get('gender') #체크박스를 아무것도 선택하지 않았을때 값이없기때문에 에러가남.
-        level = request.POST.get('level') # request.POST.get('키값')으로 받아오면 none값이 뜸
-        print(gender, level)
+    select = json.loads(request.body.decode('utf-8'))
+    print(select)
 
-        msg = check_blank(username,password,nickname,gender,level) #빈칸확인함수
-        if msg != '통과':
-            print(msg)
-            return JsonResponse({'error': msg})
-        else:#빈칸이 아니면
-            founduser= User.objects.filter(username=username)
-            if len(founduser) > 0: #같은아이디가 있을때.
-                return JsonResponse({'error': '해당아이디는 이미 존재합니다.'}) #이거 표시할 곳 필요
-            else: #중복아이디가 아니면
-                result = create_user(username,password,nickname,gender,level)
-                auth.login(request, result) #ajax가 응답을 기다릴까봐 걱정.....
-                return redirect('main')
+    # if request.method == 'POST':
+    username = select['username']
+    print(username)
+    password = select['password']
+    print(password)
+    nickname = select['nickname']
+    print(nickname)
+    gender = select['gender']
+    print(gender)
+    level = select['level']
+    print(level)
+
+    msg = check_blank(username,password,nickname,gender,level) #빈칸확인함수
+    print(msg)
+
+    if msg != '통과':
+        # print(msg)
+        return JsonResponse({'error': msg})
+    else:#빈칸이 아니면
+        founduser= User.objects.filter(username=username)
+        if len(founduser) > 0: #같은아이디가 있을때.
+            return JsonResponse({'error': '해당아이디는 이미 존재합니다.'}) #이거 표시할 곳 필요
+        else: #중복아이디가 아니면
+            result = create_user(username,password,nickname,gender,level)
+            auth.login(request, result) #ajax가 응답을 기다릴까봐 걱정.....
+            return 0
 
 
 def sign_in(request):
