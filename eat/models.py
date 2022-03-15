66c579fe9df56200c518cb75ca7bb0fa9511f1e0
rookieno <rhkdals3257@naver.com>
2022-03-15 10:15:07 +0900

    eat models

--- eat/models.py
+++ eat/models.py
@@ -4,3 +4,8 @@ from django.db import models
 class FoodModel(models.Model):
     class Meta:
         db_table = "food"
+
+    title = models.CharField(max_length=256)
+    ingredients = models.TextField()
+    image = models.URLField()
+    step = models.TextField()
