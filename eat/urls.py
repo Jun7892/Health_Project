82c0409949a75929c2440d27fda119f84bce2555
rookieno <rhkdals3257@naver.com>
2022-03-16 10:19:08 +0900

    mm

--- eat/urls.py
+++ eat/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.eat_view, name='eat'),
-    # path('detail/<int:pk>', views.eat_detial, name='eat_detail')
+    path('detail/<int:id>', views.eat_detial, name='eat_detail')
 ]
--- eat/views.py
+++ eat/views.py
@@ -8,3 +8,8 @@ def eat_view(request):
         recipe = FoodModel.objects.all()
         recipe = random.sample(list(recipe), 12)
         return render(request, 'eat/eat.html', {'recipe': recipe})
+
+
+def eat_detail(request, id):
+    recipe = FoodModel.objects.fillter(id=id)
+
