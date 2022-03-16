9fe8d79a852613ba921893428de15e5ad2f25c8d
geang <hsegeang@gmail.com>
2022-03-16 17:37:41 +0900

    rds 설정

--- plz/settings.py
+++ plz/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'plz.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sys',
+        'USER': 'tenjangsaeng',
+        'PASSWORD': '$sparta10',
+        'HOST': 'sparta-10team-database.cnwk3pjbkgr6.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
 
