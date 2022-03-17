6754fcd9ad4a96dca802c4f36504293a26b9e6ed
rookieno <rhkdals3257@naver.com>
2022-03-17 13:23:43 +0900

    up

--- commu/views.py
+++ commu/views.py
@@ -7,15 +7,15 @@ from .models.article import Article
 def commu_view(request):
     if request.method == 'GET':
         article = Article.objects.all().order_by('-created_at')
-        return render(request, 'commu/commu.html', {'article': article})
+        return render(request, 'commu/../templates/commu.html', {'article': article})
     if request.method == 'POST':
         author = request.user
         title = request.POST.get()
         content = request.POST.get()
         my_article = Article.objects.create(author=author, title=title, content=content)
         my_article.save()
-        return render(request, 'commu/commu.html')
+        return render(request, 'commu/../templates/commu.html')
 
 
-def delete_an_article(request): # 글 삭제
+def delete_an_article(request, id): # 글 삭제
     pass
