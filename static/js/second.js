aed729410237fab58f11157b1729053e97c9b582
gaeul <e0540@hanmail.net>
2022-03-21 18:17:27 +0900

    csrf토큰 base.js로 옮김

--- static/js/second.js
+++ static/js/second.js
@@ -1,34 +1,3 @@
-//csrf token
-function getCookie(name) {
-    var cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-        var cookies = document.cookie.split(';');
-        for (var i = 0; i < cookies.length; i++){
-            var cookie = cookies[i].trim();
-            // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
-    }
-    return cookieValue;
-}
-var csrftoken = getCookie('csrftoken');
-
-function csrfSafeMethod(method) {
-  // these HTTP methods do not require CSRF protection
-  return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
-}
-$.ajaxSetup({
-  beforeSend: function(xhr, settings) {
-      if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
-          xhr.setRequestHeader("X-CSRFToken", csrftoken);
-      }
-  },
-  headers: { "X-CSRFToken": '{{csrf_token}}' },
-});
-
 // 버튼 클릭시 display: flex 값 / 닫기 버튼 클릭시 modal display none 값
 const modal = document.getElementById("modal")
 
