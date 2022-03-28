3812f3a09773a5635bb56c76f505f80a234ce5e7
gaeul <e0540@hanmail.net>
2022-03-28 18:16:38 +0900

    마이페이지 검색기능 추가

--- second/urls.py
+++ second/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('find_id', views.find_id, name='find_id'),
     path('password_reset/', views.password_reset_mailing, name='password_reset_mailing'),
     path('password_reset/done/', views.email_send_success, name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', views.reset_password,name='reset_password'),
+    path('reset/<uidb64>/<token>/', views.reset_password, name='reset_password'),
+    path('delete/<int:id>', views.user_delete, name='user_delete'),
 ]
