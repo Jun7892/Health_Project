ac0d0fa6101221f77b194b5eb04b670eb5d8ad1a
rookieno <rhkdals3257@naver.com>
2022-03-29 19:46:08 +0900

    tabs city product price

--- eat/views.py
+++ eat/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from eat.models import FoodModel
+from eat.models import FoodModel, ProductModel
 from django.core.paginator import Paginator
 import random
 import pandas as pd
@@ -13,13 +13,47 @@ import json
 def eat_view(request):
     if request.method == 'GET':
         recipe = FoodModel.objects.all().order_by('id')
+        code = ['서울','부산','대구','인천','광주','대전','울산','수원','춘천','청주','전주','포항','제주','순천','안동','창원']
+        seoul = ProductModel.objects.filter(country_name='서울')
+        busan = ProductModel.objects.filter(country_name='부산')
+        daegu = ProductModel.objects.filter(country_name='대구')
+        incheon = ProductModel.objects.filter(country_name='인천')
+        gwangju = ProductModel.objects.filter(country_name='광주')
+        daejeon = ProductModel.objects.filter(country_name='대전')
+        ulsan = ProductModel.objects.filter(country_name='울산')
+        suwon = ProductModel.objects.filter(country_name='수원')
+        chuncheon = ProductModel.objects.filter(country_name='춘천')
+        chungju = ProductModel.objects.filter(country_name='청주')
+        jeonju = ProductModel.objects.filter(country_name='전주')
+        pohang = ProductModel.objects.filter(country_name='포항')
+        jeju = ProductModel.objects.filter(country_name='제주')
+        suncheon = ProductModel.objects.filter(country_name='순천')
+        andong = ProductModel.objects.filter(country_name='안동')
+        changwon = ProductModel.objects.filter(country_name='창원')
+
         page = request.GET.get('page')
         paginator = Paginator(recipe, '12')
         page_obj = paginator.get_page(page)
         context = {
             'page': page_obj,
+            'seoul': seoul,
+            'busan': busan,
+            'daegu': daegu,
+            'incheon':incheon,
+            'gwangju': gwangju,
+            'daejeon':daejeon,
+            'ulsan':ulsan,
+            'suwon':suwon,
+            'chuncheon':chuncheon,
+            'chungju':chungju,
+            'jeonju':jeonju,
+            'pohang':pohang,
+            'jeju':jeju,
+            'suncheon':suncheon,
+            'andong':andong,
+            'changwon':changwon,
+            'code':code
         }
-        # product = get_city_product_price()
         return render(request, 'eat/eat.html', context)
 
 
@@ -62,46 +96,46 @@ def bookmark(request,id):
     return redirect(f'/eat/detail/{id}')
 
 
-def city_select():
-    code = {
-          '서울':'1101',
-          '부산':'2100',
-          '대구':'2200',
-          '인천':'2300',
-          '광주':'2401',
-          '대전':'2501',
-          '울산':'2601',
-          '수원':'3111',
-          '춘천':'3211',
-          '청주':'3311',
-          '전주':'3511',
-          '포항':'3711',
-          '제주':'3911',
-          '순천':'3613',
-          '안동':'3714',
-          '창원':'3814'
-          }
-    params = []
-    for i,j in code.items():
-        a = {"p_cert_key":"50ae0e04-be51-4bf2-a7a1-ee6ac5062542", "p_cert_id":"2371", "p_returntype":"json", "p_countycode":f"{j}"}
-        params.append(a)
-
-    return params
-
-
-def get_city_product_price():
-    params_list = city_select()
-    city_product_price = []
-    for params in params_list:
-      url = 'http://www.kamis.or.kr/service/price/xml.do?action=dailyCountyList'
-      response = requests.get(url, params=params)
-      df = response.json()
-      target = df['price']
-
-      for idx in range(len(target)):
-        product_price = [target[idx]['county_name'], target[idx]['item_name'], target[idx]['day1'], target[idx]['dpr1'], target[idx]['day2'], target[idx]['dpr2'], target[idx]['value'], target[idx]['unit']]
-        city_product_price.append(product_price)
-
-    return city_product_price
+# def city_select():
+#     code = {
+#           '서울':'1101',
+#           '부산':'2100',
+#           '대구':'2200',
+#           '인천':'2300',
+#           '광주':'2401',
+#           '대전':'2501',
+#           '울산':'2601',
+#           '수원':'3111',
+#           '춘천':'3211',
+#           '청주':'3311',
+#           '전주':'3511',
+#           '포항':'3711',
+#           '제주':'3911',
+#           '순천':'3613',
+#           '안동':'3714',
+#           '창원':'3814'
+#           }
+#     params = []
+#     for i,j in code.items():
+#         a = {"p_cert_key":"50ae0e04-be51-4bf2-a7a1-ee6ac5062542", "p_cert_id":"2371", "p_returntype":"json", "p_countycode":f"{j}"}
+#         params.append(a)
+#
+#     return params
+#
+#
+# def get_city_product_price():
+#     params_list = city_select()
+#     city_product_price = []
+#     for params in params_list:
+#       url = 'http://www.kamis.or.kr/service/price/xml.do?action=dailyCountyList'
+#       response = requests.get(url, params=params)
+#       df = response.json()
+#       target = df['price']
+#
+#       for idx in range(len(target)):
+#         product_price = [target[idx]['county_name'], target[idx]['item_name'], target[idx]['day1'], target[idx]['dpr1'], target[idx]['day2'], target[idx]['dpr2'], target[idx]['value'], target[idx]['unit']]
+#         city_product_price.append(product_price)
+#
+#     return city_product_price
 
 
