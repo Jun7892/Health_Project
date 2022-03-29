e39b7f7ae589d726efe294b03dda6bae549eef05
rookieno <rhkdals3257@naver.com>
2022-03-29 18:22:27 +0900

    city_product_price

--- plz/settings.py
+++ plz/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'plz.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'mydatabase',
+        'NAME': 'db.sqlite3',
     }
 }
 
--- static/js/eat.js
+++ static/js/eat.js
@@ -330,3 +330,16 @@ function modalOff4() {
 // 	render();
 // };
 
+$(document).ready(function(){
+
+	$('ul.tabs li').click(function(){
+		var tab_id = $(this).attr('data-tab');
+
+		$('ul.tabs li').removeClass('current');
+		$('.tab-content').removeClass('current');
+
+		$(this).addClass('current');
+		$("#"+tab_id).addClass('current');
+	})
+
+})
