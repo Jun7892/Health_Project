2d5b333aa692c152c12a2476e9ff7dcbfebf495c
gaeul <e0540@hanmail.net>
2022-03-31 21:11:44 +0900

    워크아웃 나이대별 작업중. 일단 유소년 시작

--- workout/urls.py
+++ workout/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.workout_view, name='workout'),
+    path('get/level_test', views.age_different_show_page, name='age_different_show_page'),
+    path('update/level/<int:id>', views.youth_and_old_level_confirm, name='youth_and_old_level_confirm'),
 ]
