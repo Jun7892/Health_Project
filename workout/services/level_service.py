70d14723711784af309497fd931f875412dc7ad3
gaeul <e0540@hanmail.net>
2022-04-04 19:48:45 +0900

    비디오 필터링에서 안걸리면 미리지정해둔 비디오에서 던지게 바꿈

--- workout/services/level_service.py
+++ workout/services/level_service.py
@@ -4,23 +4,29 @@ def boy_youth_level_service(real_age, count, user): # 유소년 남자
         if count >= 0 and count < 30: #횟수 30미만이면 3등급
             user.level = '3등급'
             user.save()
+            return '3등급'
         elif 30 <= count < 33:
             user.level = '2등급'
             user.save()
+            return '2등급'
         elif count >= 33:
             user.level = '1등급'
             user.save()
+            return '1등급'
 
     else: #12살이면
         if 0 <= count < 29:  # 횟수 30미만이면 3등급
             user.level = '3등급'
             user.save()
+            return '3등급'
         elif 30 <= count < 32:
             user.level = '2등급'
             user.save()
+            return '2등급'
         elif count >= 32:
             user.level = '1등급'
             user.save()
+            return '1등급'
 
 
 def girl_youth_level_service(real_age, count, user):#유소년 여자
@@ -28,136 +34,171 @@ def girl_youth_level_service(real_age, count, user):#유소년 여자
         if 0 <= count < 30: #횟수 30미만이면 3등급
             user.level = '3등급'
             user.save()
+            return '3등급'
         elif 30 <= count < 32:
             user.level = '2등급'
             user.save()
+            return '2등급'
         elif count >= 33:
             user.level = '1등급'
             user.save()
+            return '1등급'
 
     else: #12살이면
         if 0 <= count < 30:  # 횟수 30미만이면 3등급
             user.level = '3등급'
             user.save()
+            return '3등급'
         elif 30 <= count < 33:
             user.level = '2등급'
             user.save()
+            return '2등급'
         elif count >= 34:
             user.level = '1등급'
             user.save()
-
+            return '1등급'
 
 def man_senior_level_service(real_age, count, user):
     if 65 <= real_age < 70:#65~69세
         if 0 <= count < 21:
             user.level = '3등급'
             user.save()
+            return '3등급'
         elif 21 <= count < 25:
             user.level = '2등급'
             user.save()
+            return '2등급'
         elif count >= 25:
             user.level = '1등급'
             user.save()
+            return '1등급'
 
     elif 70 <= real_age < 75:
         if 0 <= count < 20:
             user.level = '3등급'
             user.save()
+            return '3등급'
         elif 20 <= count < 23:
             user.level = '2등급'
             user.save()
+            return '2등급'
         elif count >= 23:
             user.level = '1등급'
             user.save()
+            return '1등급'
 
     elif 75 <= real_age < 80:
         if 0 <= count < 17:
             user.level = '3등급'
             user.save()
+            return '3등급'
         elif 17 <= count < 21:
             user.level = '2등급'
             user.save()
+            return '2등급'
         elif count >= 21:
             user.level = '1등급'
             user.save()
+            return '1등급'
 
     elif 80 <= real_age < 85:
         if 0 <= count < 15:
             user.level = '3등급'
             user.save()
+            return '3등급'
         elif 15 <= count < 19:
             user.level = '2등급'
             user.save()
+            return '2등급'
         elif count >= 19:
             user.level = '1등급'
             user.save()
+            return '1등급'
 
     else: # 85세 이상
         if 0 <= count < 13:
             user.level = '3등급'
             user.save()
+            return '3등급'
         elif 13 <= count < 17:
             user.level = '2등급'
             user.save()
+            return '2등급'
         elif count >= 17:
             user.level = '1등급'
             user.save()
+            return '1등급'
 
 def woman_senior_level_service(real_age, count, user):
     if 65 <= real_age < 70:#65~69세
         if 0 <= count < 19:
             user.level = '3등급'
             user.save()
+            return '3등급'
         elif 19 <= count < 22:
             user.level = '2등급'
             user.save()
+            return '2등급'
         elif count >= 22:
             user.level = '1등급'
             user.save()
+            return '1등급'
 
     elif 70 <= real_age < 75:
         if 0 <= count < 17:
             user.level = '3등급'
             user.save()
+            return '3등급'
         elif 17 <= count < 20:
             user.level = '2등급'
             user.save()
+            return '2등급'
         elif count >= 20:
             user.level = '1등급'
             user.save()
+            return '1등급'
 
     elif 75 <= real_age < 80:
         if 0 <= count < 15:
             user.level = '3등급'
             user.save()
+            return '3등급'
         elif 15 <= count < 18:
             user.level = '2등급'
             user.save()
+            return '2등급'
         elif count >= 18:
             user.level = '1등급'
             user.save()
+            return '1등급'
 
     elif 80 <= real_age < 85:
         if 0 <= count < 13:
             user.level = '3등급'
             user.save()
+            return '3등급'
         elif 13 <= count < 15:
             user.level = '2등급'
             user.save()
+            return '2등급'
         elif count >= 15:
             user.level = '1등급'
             user.save()
+            return '1등급'
 
     else: # 85세 이상
         if 0 <= count < 11:
             user.level = '3등급'
             user.save()
+            return '3등급'
         elif 11 <= count < 14:
             user.level = '2등급'
             user.save()
+            return '2등급'
         elif 14 <= count:
             user.level = '1등급'
             user.save()
+            return '1등급'
 
 
 def boy_student_level_service(real_age, count, user):
@@ -165,67 +206,85 @@ def boy_student_level_service(real_age, count, user):
         if 0 <= count < 49:
             user.level = '3등급'
             user.save()
+            return '3등급'
         elif 49 <= count < 61:
             user.level = '2등급'
             user.save()
+            return '2등급'
         elif 61 <= count:
             user.level = '1등급'
             user.save()
+            return '1등급'
 
     elif real_age == 14:
         if 0 <= count < 48:
             user.level = '3등급'
             user.save()
+            return '3등급'
         elif 48 <= count < 60:
             user.level = '2등급'
             user.save()
+            return '2등급'
         elif 60 <= count:
             user.level = '1등급'
             user.save()
+            return '1등급'
 
     elif real_age == 15:
         if 0 <= count < 39:
             user.level = '3등급'
             user.save()
+            return '3등급'
         elif 39 <= count < 49:
             user.level = '2등급'
             user.save()
+            return '2등급'
         elif 49 <= count:
             user.level = '1등급'
             user.save()
+            return '1등급'
 
     elif real_age == 16:
         if 0 <= count < 38:
             user.level = '3등급'
             user.save()
+            return '3등급'
         elif 38 <= count < 49:
             user.level = '2등급'
             user.save()
+            return '2등급'
         elif 49 <= count:
             user.level = '1등급'
             user.save()
+            return '1등급'
 
     elif real_age == 17:
         if 0 <= count < 36:
             user.level = '3등급'
             user.save()
+            return '3등급'
         elif 36 <= count < 46:
             user.level = '2등급'
             user.save()
+            return '2등급'
         elif 46 <= count:
             user.level = '1등급'
             user.save()
+            return '1등급'
 
     elif real_age == 18:
         if 0 <= count < 41:
             user.level = '3등급'
             user.save()
+            return '3등급'
         elif 41 <= count < 53:
             user.level = '2등급'
             user.save()
+            return '2등급'
         elif 53 <= count:
             user.level = '1등급'
             user.save()
+            return '1등급'
 
 
 
@@ -234,156 +293,198 @@ def girl_student_level_service(real_age, count, user):
         if 0 <= count < 37:
             user.level = '3등급'
             user.save()
+            return '3등급'
         elif 37 <= count < 48:
             user.level = '2등급'
             user.save()
+            return '2등급'
         elif 48 <= count:
             user.level = '1등급'
             user.save()
+            return '1등급'
 
     elif real_age == 14:
         if 0 <= count < 36:
             user.level = '3등급'
             user.save()
+            return '3등급'
         elif 36 <= count < 47:
             user.level = '2등급'
             user.save()
+            return '2등급'
         elif 47 <= count:
             user.level = '1등급'
             user.save()
+            return '1등급'
 
     elif real_age == 15:
         if 0 <= count < 36:
             user.level = '3등급'
             user.save()
+            return '3등급'
         elif 36 <= count < 48:
             user.level = '2등급'
             user.save()
+            return '2등급'
         elif 48 <= count:
             user.level = '1등급'
             user.save()
+            return '1등급'
 
     elif real_age == 16:
         if 0 <= count < 30:
             user.level = '3등급'
             user.save()
+            return '3등급'
         elif 30 <= count < 41:
             user.level = '2등급'
             user.save()
+            return '2등급'
         elif 41 <= count:
             user.level = '1등급'
             user.save()
+            return '1등급'
 
     elif real_age == 17 or real_age == 18:
         if 0 <= count < 29:
             user.level = '3등급'
             user.save()
+            return '3등급'
         elif 30 <= count < 41:
             user.level = '2등급'
             user.save()
+            return '2등급'
         elif 41 <= count:
             user.level = '1등급'
             user.save()
+            return '1등급'
 
 def man_adult_level_service(real_age, count, user):
     if 19 <= real_age < 25:
         if 0<= count < 48:
             user.level = '3등급'
             user.save()
+            return '3등급'
         elif 48 <= count < 55:
             user.level = '2등급'
             user.save()
+            return '2등급'
         elif 55 <= count:
             user.level = '1등급'
             user.save()
+            return '1등급'
 
     elif 25 <= real_age < 30:
         if 0 <= count < 45:
             user.level = '3등급'
             user.save()
+            return '3등급'
         elif 45 <= count < 51:
             user.level = '2등급'
             user.save()
+            return '2등급'
         elif 51 <= count:
             user.level = '1등급'
             user.save()
+            return '1등급'
 
     elif 30 <= real_age < 35:
         if 0 <= count < 41:
             user.level = '3등급'
             user.save()
+            return '3등급'
         elif 41 <= count < 47:
             user.level = '2등급'
             user.save()
+            return '2등급'
         elif 47 <= count:
             user.level = '1등급'
             user.save()
+            return '1등급'
 
     elif 35 <= real_age < 40:
         if 0 <= count < 39:
             user.level = '3등급'
             user.save()
+            return '3등급'
         elif 39 <= count < 45:
             user.level = '2등급'
             user.save()
+            return '2등급'
         elif 45 <= count:
             user.level = '1등급'
             user.save()
+            return '1등급'
 
     elif 40 <= real_age < 45:
         if 0 <= count < 38:
             user.level = '3등급'
             user.save()
+            return '3등급'
         elif 38 <= count < 44:
             user.level = '2등급'
             user.save()
+            return '2등급'
         elif 44 <= count:
             user.level = '1등급'
             user.save()
+            return '1등급'
 
     elif 45 <= real_age < 50:
         if 0 <= count < 36:
             user.level = '3등급'
             user.save()
+            return '3등급'
         elif 36 <= count < 41:
             user.level = '2등급'
             user.save()
+            return '2등급'
         elif 41 <= count:
             user.level = '1등급'
             user.save()
+            return '1등급'
 
     elif 50 <= real_age < 55:
         if 0 <= count < 32:
             user.level = '3등급'
             user.save()
+            return '3등급'
         elif 32 <= count < 38:
             user.level = '2등급'
             user.save()
+            return '2등급'
         elif 38 <= count:
             user.level = '1등급'
             user.save()
+            return '1등급'
 
     elif 55 <= real_age < 60:
         if 0 <= count < 29:
             user.level = '3등급'
             user.save()
+            return '3등급'
         elif 29 <= count < 35:
             user.level = '2등급'
             user.save()
+            return '2등급'
         elif 35 <= count:
             user.level = '1등급'
             user.save()
+            return '1등급'
 
     elif 60 <= real_age < 65:
         if 0 <= count < 25:
             user.level = '3등급'
             user.save()
+            return '3등급'
         elif 25 <= count < 31:
             user.level = '2등급'
             user.save()
+            return '2등급'
         elif 31 <= count:
             user.level = '1등급'
             user.save()
+            return '1등급'
 
 
 def woman_adult_level_service(real_age, count, user):
@@ -391,86 +492,110 @@ def woman_adult_level_service(real_age, count, user):
         if 0<= count < 30:
             user.level = '3등급'
             user.save()
+            return '3등급'
         elif 30 <= count < 36:
             user.level = '2등급'
             user.save()
+            return '2등급'
         elif 36 <= count:
             user.level = '1등급'
             user.save()
+            return '1등급'
 
     elif 25 <= real_age < 30:
         if 0 <= count < 27:
             user.level = '3등급'
             user.save()
+            return '3등급'
         elif 27 <= count < 33:
             user.level = '2등급'
             user.save()
+            return '2등급'
         elif 33 <= count:
             user.level = '1등급'
             user.save()
+            return '1등급'
 
     elif 30 <= real_age < 35 or 35 <= real_age < 40:
         if 0 <= count < 25:
             user.level = '3등급'
             user.save()
+            return '3등급'
         elif 25<= count < 31:
             user.level = '2등급'
             user.save()
+            return '2등급'
         elif 31 <= count:
             user.level = '1등급'
             user.save()
+            return '1등급'
 
     elif 40 <= real_age < 45:
         if 0 <= count < 25:
             user.level = '3등급'
             user.save()
+            return '3등급'
         elif 25<= count < 30:
             user.level = '2등급'
             user.save()
+            return '2등급'
         elif 30 <= count:
             user.level = '1등급'
             user.save()
+            return '1등급'
 
     elif 45 <= real_age < 50:
         if 0 <= count < 22:
             user.level = '3등급'
             user.save()
+            return '3등급'
         elif 22<= count < 24:
             user.level = '2등급'
             user.save()
+            return '2등급'
         elif 34 <= count:
             user.level = '1등급'
             user.save()
+            return '1등급'
 
     elif 50 <= real_age < 55:
         if 0 <= count < 19:
             user.level = '3등급'
             user.save()
+            return '3등급'
         elif count == 19:
             user.level = '2등급'
             user.save()
+            return '2등급'
         elif 20 <= count:
             user.level = '1등급'
             user.save()
+            return '1등급'
 
     elif 55 <= real_age < 60:
         if 0 <= count < 15:
             user.level = '3등급'
             user.save()
+            return '3등급'
         elif 15 <= count < 20:
             user.level = '2등급'
             user.save()
+            return '2등급'
         elif 20 <= count:
             user.level = '1등급'
             user.save()
+            return '1등급'
 
     elif 60 <= real_age < 65:
         if 0 <= count < 12:
             user.level = '3등급'
             user.save()
+            return '3등급'
         elif 12<= count < 17:
             user.level = '2등급'
             user.save()
+            return '2등급'
         elif 17 <= count:
             user.level = '1등급'
             user.save()
+            return '1등급'
--- workout/views.py
+++ workout/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect
+from django.utils.datastructures import MultiValueDictKeyError
+
 from workout.models import Exercise
 from workout.services.level_service import boy_youth_level_service, girl_youth_level_service, man_senior_level_service, \
     woman_senior_level_service, boy_student_level_service, girl_student_level_service, man_adult_level_service, \
     woman_adult_level_service
 from second.models import User
-from workout.services.make_random_warmup import make_youth_random_warmup, make_random_others_warmup, \
-    make_random_main, get_main_video_id, get_warm_up_video_id
+from workout.services.make_random_warmup import make_youth_random_warmup,make_random_main, get_main_video_id, get_warm_up_video_id, make_random_others_warmup
 
 
 def workout_view(request):
@@ -17,7 +18,7 @@ def workout_view(request):
         else: #유저 등급주어졌다면 운동페이지 보여줘야해
             user_match_exercise_list= Exercise.objects.filter(gender=who.gender, level=who.level,age=who.age)
             picked_exercise =user_match_exercise_list.order_by('?').first()
-            # picked_exercise=Exercise.objects.get(id=2528)
+            # picked_exercise=Exercise.objects.get(id=2833)
             user_warm_up = picked_exercise.get_warm_up() #get_warm_up으로 꺼내야 한글로나옴
             user_main = picked_exercise.get_main()
             warm_up_db_list = user_warm_up.split(',')
@@ -33,42 +34,58 @@ def workout_view(request):
                 if who.age == '유소년':
                     if who.level == '3등급':
                         warm_up_list= make_youth_random_warmup(grade)
-                        #본운동은 있는거 던져줘야지
                         main_list= get_main_video_id(main_db_list, who.age)
+                        if len(main_list) == 0:
+                            main_list = make_random_main(who.age, grade)
                         return render(request, 'workout.html', {'warm_up_list': warm_up_list ,'main_list':main_list})
                     elif who.level =='2등급':
                         warm_up_list= make_youth_random_warmup(grade)
-                        # 본운동은 있는거 던져줘야지
                         main_list= get_main_video_id(main_db_list, who.age)
+                        if len(main_list) == 0:
+                            main_list = make_random_main(who.age, grade)
                         return render(request, 'workout.html', {'warm_up_list': warm_up_list ,'main_list':main_list})
                     else: #유소년 1등급
                         warm_up_list = make_youth_random_warmup(grade)
                         main_list= get_main_video_id(main_db_list, who.age)
+                        if len(main_list) == 0:
+                            main_list = make_random_main(who.age, grade)
                         return render(request, 'workout.html', {'warm_up_list': warm_up_list, 'main_list': main_list})
-                else:
+                else: #청소년/성인/노인
                     warm_up_list = make_random_others_warmup(who.age)
                     main_list = get_main_video_id(main_db_list, who.age)
+                    if len(main_list) == 0:
+                        main_list = make_random_main(who.age, grade)
                     return render(request, 'workout.html', {'warm_up_list': warm_up_list, 'main_list': main_list})
             #본운동이 없을때
             elif len(user_main) == 0:
                 #준비운동은 DB에있는거 그대로 던져주고, 본운동은 기존 나이대/등급별 리스트 랜덤던져줌
                 warm_up_list = get_warm_up_video_id(warm_up_db_list, who.age)
-                if warm_up_list == 0:
+                main_list = make_random_main(who.age, grade)
+                if len(warm_up_list) == 0:
                     if who.age == '유소년':
                         warm_up_list = make_youth_random_warmup(grade)
-                main_list = make_random_main(who.age, grade)
+                    else:#청소년/성인/노인
+                        warm_up_list=make_random_others_warmup(who.age)
                 return render(request, 'workout.html', {'warm_up_list': warm_up_list, 'main_list': main_list})
 
             else: # 준비운동 있고, 본운동 있고
                 warm_up_list = get_warm_up_video_id(warm_up_db_list, who.age)
-                if len(warm_up_list) == 0:
+                main_list = get_main_video_id(main_db_list, who.age)
+                if len(warm_up_list) == 0 and len(main_list) == 0:
                     if who.age == '유소년':
                         warm_up_list = make_youth_random_warmup(grade)
-                main_list = get_main_video_id(main_db_list, who.age)
-                if len(main_list) == 0:
-                    make_random_main(who.age, grade)
-                print(warm_up_list,main_list)
-
+                        main_list = make_random_main(who.age, grade)
+                    else:#청소년/성인/노인
+                        warm_up_list = make_random_others_warmup(who.age)
+                        main_list = make_random_main(who.age, grade)
+                elif len(warm_up_list) == 0:
+                    if who.age == '유소년':
+                        warm_up_list = make_youth_random_warmup(grade)
+                    else:#청소년/성인/노인
+                        warm_up_list = make_random_others_warmup(who.age)
+                elif len(main_list) == 0:
+                    main_list= make_random_main(who.age, grade)
+                print(warm_up_list,main_list, '준비운동길이',len(warm_up_list), '메인운동길이',len(main_list))
                 return render(request, 'workout.html', {'warm_up_list': warm_up_list,'main_list': main_list})
 
 #측정후에도 다시하고싶을수도 있으니까 나이대별 측정페이지 보여줄수 있음
@@ -88,38 +105,42 @@ def level_confirm(request, id):
     user = User.objects.get(id=id)
     age_range = user.age
     real_age = user.first_name
-    count = request.POST['count']
     if request.method == 'POST':
-        if login_user == user:
-            if age_range == '유소년':
-                if user.gender == '남자':
-                    boy_youth_level_service(int(real_age), int(count), user) # 등급여기서 나눠서 저장해줌
-                    return render(request, 'workout/show_level.html', {'user': login_user})
-                elif user.gender == '여자':
-                    girl_youth_level_service(int(real_age), int(count), user)
-                    return render(request, 'workout/show_level.html', {'user': login_user})
-            elif age_range == '청소년':
-                if user.gender == '남자':
-                    boy_student_level_service(int(real_age), int(count), user)  # 등급여기서 나눠서 저장해줌
-                    return render(request, 'workout/show_level.html', {'user': login_user})
-                elif user.gender == '여자':
-                    girl_student_level_service(int(real_age), int(count), user)
-                    return render(request, 'workout/show_level.html', {'user': login_user})
-            elif age_range == '성인':
-                if user.gender == '남자':
-                    man_adult_level_service(int(real_age), int(count), user)
-                    return render(request, 'workout/show_level.html', {'user': login_user})
-                elif user.gender == '여자':
-                    woman_adult_level_service(int(real_age), int(count), user)
-                    return render(request, 'workout/show_level.html', {'user': login_user})
-            elif age_range == '노인':
-                if user.gender == '남자':
-                    man_senior_level_service(int(real_age), int(count), user)
-                    return render(request, 'workout/show_level.html', {'user': login_user})
-                elif user.gender == '여자':
-                    woman_senior_level_service(int(real_age), int(count), user)
-                    return render(request, 'workout/show_level.html', {'user': login_user})
-        else:
-            return redirect('age_different_show_page')
+        try:
+            count = request.POST['count']
+            if login_user == user:
+                if age_range == '유소년':
+                    if user.gender == '남자':
+                        grade = boy_youth_level_service(int(real_age), int(count), user) # 등급여기서 나눠서 저장해줌
+                        return render(request, 'workout/show_level.html', {'grade': grade,'user': login_user})
+                    elif user.gender == '여자':
+                        grade = girl_youth_level_service(int(real_age), int(count), user)
+                        return render(request, 'workout/show_level.html', {'grade': grade,'user': login_user})
+                elif age_range == '청소년':
+                    if user.gender == '남자':
+                        grade = boy_student_level_service(int(real_age), int(count), user)  # 등급여기서 나눠서 저장해줌
+                        return render(request, 'workout/show_level.html', {'grade': grade,'user': login_user})
+                    elif user.gender == '여자':
+                        grade = girl_student_level_service(int(real_age), int(count), user)
+                        return render(request, 'workout/show_level.html', {'grade': grade,'user': login_user})
+                elif age_range == '성인':
+                    if user.gender == '남자':
+                        grade = man_adult_level_service(int(real_age), int(count), user)
+                        return render(request, 'workout/show_level.html', {'grade': grade,'user': login_user})
+                    elif user.gender == '여자':
+                        grade = woman_adult_level_service(int(real_age), int(count), user)
+                        return render(request, 'workout/show_level.html', {'grade': grade,'user': login_user})
+                elif age_range == '노인':
+                    if user.gender == '남자':
+                        grade = man_senior_level_service(int(real_age), int(count), user)
+                        return render(request, 'workout/show_level.html', {'grade': grade,'user': login_user})
+                    elif user.gender == '여자':
+                        grade = woman_senior_level_service(int(real_age), int(count), user)
+                        return render(request, 'workout/show_level.html', {'grade': grade,'user': login_user})
+            else:
+                return redirect('age_different_show_page')
+        except MultiValueDictKeyError(KeyError): # 등급받고서 또 주소창에서 엔터치면
+            return redirect('workout')
     else:
-        return redirect('age_different_show_page')
+        print('여기지?')
+        return redirect('workout')
