fedb93e4f2b7ba49ddcec806069960b1256d6859
gaeul <e0540@hanmail.net>
2022-04-04 17:52:54 +0900

    운동처방전 에 맞게 동영상 띄우기

---
+++ workout/services/make_random_warmup.py
@@ -0,0 +1,168 @@
+import random
+
+from workout.models import Video_id
+
+
+def make_youth_random_warmup(grade):
+    if grade == '3':
+        warm_up_list = []
+        youth3_warmup_list = ['13pReCtwYgM', 'cjQJXqVguGY', 'cXQJEYiRnUM', 'ZmUj-E9A44E']
+        random_index = random.randrange(1, len(youth3_warmup_list))
+        warmup_video_id = youth3_warmup_list[random_index]
+        warm_up_list.append(warmup_video_id)
+        return warm_up_list
+            #성인/노인/ 청소년은 준비운동 같음
+
+    elif grade == '2':
+        warm_up_list = []
+        youth2_warmup_list = ['13pReCtwYgM','cjQJXqVguGY','cXQJEYiRnUM','J79-J2aQa0s','ZmUj-E9A44E']
+        random_index = random.randrange(1, len(youth2_warmup_list))
+        warmup_video_id = youth2_warmup_list[random_index]
+        warm_up_list.append(warmup_video_id)
+        return warm_up_list
+
+    else: #1등급
+        warm_up_list = []
+        youth1_warmup_list = ['13pReCtwYgM','J79-J2aQa0s','ZmUj-E9A44E','ipB3TD0BpwA']
+        random_index = random.randrange(1, len(youth1_warmup_list))
+        warmup_video_id = youth1_warmup_list[random_index]
+        warm_up_list.append(warmup_video_id)
+        return warm_up_list
+
+
+def make_random_others_warmup(age):
+    if age == '청소년':
+        warm_up_list = []
+        student_warmup_list = ['dsKW-rdjPwA','99D1pWmpBeI','tauWsdpWqI4','ilGxwE4G4-E','0k32Xj_CKk4']
+        random_index = random.randrange(1, len(student_warmup_list))
+        warmup_video_id = student_warmup_list[random_index]
+        warm_up_list.append(warmup_video_id)
+        return warm_up_list
+    elif age == '성인':
+        warm_up_list = []
+        adult_warmup_list = ['dsKW-rdjPwA','Tvw42auu5N4','UKQEojC4G9k','_bqes3Cw5ug','yyz_iOD_ZH0']
+        random_index = random.randrange(1, len(adult_warmup_list))
+        warmup_video_id = adult_warmup_list[random_index]
+        warm_up_list.append(warmup_video_id)
+        return warm_up_list
+    else: #노인
+        warm_up_list = []
+        senior_warmup_list = ['HzAk4tYsLyU','lq1c54VERbo','Q57q79bL-90','B2OmkH0UNuc','Msubsqnqeo4']
+        random_index = random.randrange(1, len(senior_warmup_list))
+        warmup_video_id = senior_warmup_list[random_index]
+        warm_up_list.append(warmup_video_id)
+        return warm_up_list
+
+
+def get_warm_up_video_id(warm_up_db_list,age):
+    warm_up_list = []
+    for i in range(len(warm_up_db_list)):  # 반복문돌리고
+        result = Video_id.objects.filter(title__contains='['+age+']'+ warm_up_db_list[i])
+        print(Video_id.objects.filter(title__contains='[' + age + ']' + warm_up_db_list[i]).exists())
+        if Video_id.objects.filter(title__contains='[' + age + ']' + warm_up_db_list[i]).exists():
+            warm_up_list.append(result[0].video_id)
+        else:
+            pass
+    return warm_up_list
+
+def get_main_video_id(main_db_list, age):
+    main_list = []
+    for i in range(len(main_db_list)):  # 반복문돌리고
+        result = Video_id.objects.filter(title__contains='['+age+']'+ main_db_list[i])
+        if Video_id.objects.filter(title__contains='[' + age + ']' + main_db_list[i]).exists():
+            main_list.append(result[0].video_id)
+        else:
+            pass
+    return main_list
+
+
+def make_random_main(age,grade):
+    if grade == '3':
+        if age == '유소년':
+            main_list = []
+            youth3_main_list = ['0k32Xj_CKk4','uLnYKa-d-Os','JmPP5cMPlGg','uLnYKa-d-Os','ZzZNhpNcaHQ']
+            random_index = random.randrange(1, len(youth3_main_list))
+            main_video_id = youth3_main_list[random_index]
+            main_list.append(main_video_id)
+            return main_list
+        elif age == '청소년':
+            main_list = []
+            student3_main_list = ['0k32Xj_CKk4','hCaIw9L_Pp8','o6ARYhR-gw8','CRMpWponGIc','P968Pd37-NQ']
+            random_index = random.randrange(1, len(student3_main_list))
+            main_video_id = student3_main_list[random_index]
+            main_list.append(main_video_id)
+            return main_list
+        elif age == '성인':
+            main_list = []
+            adult3_main_list = ['7Bickpeymew', '-O1lweMLj4E', '-O1lweMLj4E', 'c5kNdIk9jgc', 'sw_4b6GKfnI', 'DT5d9GrbLyg']
+            random_index = random.randrange(1, len(adult3_main_list))
+            main_video_id = adult3_main_list[random_index]
+            main_list.append(main_video_id)
+            return main_list
+        else:#노인3등급
+            main_list = []
+            senior3_main_list = ['_0D9iMzntJI','OiXEJ29IVPU','6ws5Ze7ootE','clifZdPnX9E','SuJKMztDB58','6ulvd_mw_uo']
+            random_index = random.randrange(1, len(senior3_main_list))
+            main_video_id = senior3_main_list[random_index]
+            main_list.append(main_video_id)
+            return main_list
+
+    elif grade == '2':
+        if age == '유소년':
+            main_list = []
+            youth2_main_list = ['0k32Xj_CKk4','uLnYKa-d-Os','oz9TL60Amb4','0V2O2VnxdCg','JmPP5cMPlGg']
+            random_index = random.randrange(1, len(youth2_main_list))
+            main_video_id = youth2_main_list[random_index]
+            main_list.append(main_video_id)
+            return main_list
+        elif age == '청소년':
+            main_list = []
+            student2_main_list = ['hCaIw9L_Pp8','L6o5qmuoxl4','o6ARYhR-gw8','NwcxlUkJems','CRMpWponGIc']
+            random_index = random.randrange(1, len(student2_main_list))
+            main_video_id = student2_main_list[random_index]
+            main_list.append(main_video_id)
+            return main_list
+        elif age == '성인':
+            main_list = []
+            adult2_main_list = ['jRK9h4VGECI','-O1lweMLj4E','-O1lweMLj4E','hCJgzZIo7cI','sw_4b6GKfnI','DT5d9GrbLyg','L6o5qmuoxl4']
+            random_index = random.randrange(1, len(adult2_main_list))
+            main_video_id = adult2_main_list[random_index]
+            main_list.append(main_video_id)
+            return main_list
+        else: #노인2등급
+            main_list = []
+            senior2_main_list = ['ll_eGqz-yH4','_0D9iMzntJI','6ws5Ze7ootE','clifZdPnX9E','SuJKMztDB58','6ulvd_mw_uo']
+            random_index = random.randrange(1, len(senior2_main_list))
+            main_video_id = senior2_main_list[random_index]
+            main_list.append(main_video_id)
+            return main_list
+
+    else: #1등급
+        if age == '유소년':
+            main_list = []
+            youth1_main_list = ['_SPkfAVwR0I','oz9TL60Amb4','0V2O2VnxdCg','JmPP5cMPlGg','3h5HD6IEjqE']
+            random_index = random.randrange(1, len(youth1_main_list))
+            main_video_id = youth1_main_list[random_index]
+            main_list.append(main_video_id)
+            return main_list
+        elif age == '청소년':
+            main_list = []
+            student1_main_list = ['hCaIw9L_Pp8','Wz-JIKxiPXk','o6ARYhR-gw8','NwcxlUkJems','CRMpWponGIc']
+            random_index = random.randrange(1, len(student1_main_list))
+            main_video_id = student1_main_list[random_index]
+            main_list.append(main_video_id)
+            return main_list
+        elif age == '성인':
+            main_list = []
+            adult1_main_list = ['f-X9yBbSNao', 'C7REZandz0g', 'owKBuruZh6o', 'DuSIEmcodeU', 'Gw73tXd_Wow']
+            random_index = random.randrange(1, len(adult1_main_list))
+            main_video_id = adult1_main_list[random_index]
+            main_list.append(main_video_id)
+            return main_list
+        else:  # 노인1등급
+            main_list = []
+            senior1_main_list = ['ll_eGqz-yH4','_0D9iMzntJI','nDxtByfXnzs','ahOYqLVAxwI','fbRvjzl_RIE','6ulvd_mw_uo','clifZdPnX9E']
+            random_index = random.randrange(1, len(senior1_main_list))
+            main_video_id = senior1_main_list[random_index]
+            main_list.append(main_video_id)
+            return main_list
--- workout/views.py
+++ workout/views.py
@@ -1,14 +1,11 @@
-import googleapiclient.errors
 from django.shortcuts import render, redirect
-import random
 from workout.models import Exercise
-from workout.services.get_youtube_video_id import get_video_list
 from workout.services.level_service import boy_youth_level_service, girl_youth_level_service, man_senior_level_service, \
     woman_senior_level_service, boy_student_level_service, girl_student_level_service, man_adult_level_service, \
     woman_adult_level_service
 from second.models import User
-from googleapiclient.errors import HttpError
-
+from workout.services.make_random_warmup import make_youth_random_warmup, make_random_others_warmup, \
+    make_random_main, get_main_video_id, get_warm_up_video_id
 
 
 def workout_view(request):
@@ -20,10 +17,14 @@ def workout_view(request):
         else: #유저 등급주어졌다면 운동페이지 보여줘야해
             user_match_exercise_list= Exercise.objects.filter(gender=who.gender, level=who.level,age=who.age)
             picked_exercise =user_match_exercise_list.order_by('?').first()
-            # picked_exercise = user_match_exercise_list[300] #준비운동 없는 친구
+            # picked_exercise=Exercise.objects.get(id=2528)
             user_warm_up = picked_exercise.get_warm_up() #get_warm_up으로 꺼내야 한글로나옴
             user_main = picked_exercise.get_main()
-            print(user_main)
+            warm_up_db_list = user_warm_up.split(',')
+            main_db_list = user_main.split(',')
+            grade = who.level.replace('등급', "")
+
+            print('어디서 오류나나보쟈',picked_exercise, warm_up_db_list,main_db_list)
             #데이터셋자체가 둘다없는 경우는 없음.
             #준비운동이 없을때
             if len(user_warm_up) == 0 :
@@ -31,95 +32,46 @@ def workout_view(request):
                 # 본운동은 데이터셋에 저장된 거 검색어로 불러오기
                 if who.age == '유소년':
                     if who.level == '3등급':
-                        youth3_warmup_list = ['13pReCtwYgM', 'cjQJXqVguGY', 'cXQJEYiRnUM', 'ZmUj-E9A44E']
-                        random_index= random.randrange(1,len(youth3_warmup_list))
-                        warmup_video_id = youth3_warmup_list[random_index]
+                        warm_up_list= make_youth_random_warmup(grade)
                         #본운동은 있는거 던져줘야지
-                        user_main
-                        return render(request, 'workout.html', {'warm_up_list': warmup_video_id })
+                        main_list= get_main_video_id(main_db_list, who.age)
+                        return render(request, 'workout.html', {'warm_up_list': warm_up_list ,'main_list':main_list})
                     elif who.level =='2등급':
-                        youth2_warmup_list = ['13pReCtwYgM','cjQJXqVguGY','cXQJEYiRnUM','J79-J2aQa0s','ZmUj-E9A44E']
-                        random_index = random.randrange(1, len(youth2_warmup_list))
-                        warmup_video_id = youth2_warmup_list[random_index]
-                        return render(request, 'workout.html', {'warm_up_list': warmup_video_id})
+                        warm_up_list= make_youth_random_warmup(grade)
+                        # 본운동은 있는거 던져줘야지
+                        main_list= get_main_video_id(main_db_list, who.age)
+                        return render(request, 'workout.html', {'warm_up_list': warm_up_list ,'main_list':main_list})
                     else: #유소년 1등급
-                        youth1_warmup_list = ['13pReCtwYgM','J79-J2aQa0s','ZmUj-E9A44E','ipB3TD0BpwA']
-                        random_index = random.randrange(1, len(youth1_warmup_list))
-                        warmup_video_id = youth1_warmup_list[random_index]
-                        return render(request, 'workout.html', {'warm_up_list': warmup_video_id})
-                elif who.age == '청소년':
-                    pass
-                elif who.age == '성인':
-                    try:
-                        print('여기?')
-                        #성인은 준비운동 다 같아서 이거에서 랜덤으로 던져주고
-
-                        adult_warmup_list = ['dsKW-rdjPwA','Tvw42auu5N4','UKQEojC4G9k','_bqes3Cw5ug','yyz_iOD_ZH0']
-                        random_index = random.randrange(1, len(adult_warmup_list))
-                        warmup_video_id = adult_warmup_list[random_index]
-                        # 본운동은 불러온거에서 주기
-                        main_db_list = user_main.split(',')
-                        print(main_db_list, len(main_db_list))
-                        main_list = []
-                        for i in range(len(main_db_list)): #반복문돌리고
-                            result = get_video_list('국민체력100_운동처방전 ' + who.age + main_db_list[i])
-                            main_list.append(result)
-                        return render(request, 'workout.html', {'warm_up_list': warmup_video_id, 'main_list': main_list})
-                    except googleapiclient.errors.HttpError: #API할당량 넘으면 이렇게됨.
-                        return render(request,'workout.html')
-
-
-                else: #노인
-                    pass
-
-                result = get_video_list('검색어로 던져줄 친구') #이래야 얘가나오네
-                video_id_list=[]
-                video_id_list.append(result)
-                # print('이게결과:',result, '얘는리스트:',video_id_list)
-
+                        warm_up_list = make_youth_random_warmup(grade)
+                        main_list= get_main_video_id(main_db_list, who.age)
+                        return render(request, 'workout.html', {'warm_up_list': warm_up_list, 'main_list': main_list})
+                else:
+                    warm_up_list = make_random_others_warmup(who.age)
+                    main_list = get_main_video_id(main_db_list, who.age)
+                    return render(request, 'workout.html', {'warm_up_list': warm_up_list, 'main_list': main_list})
             #본운동이 없을때
             elif len(user_main) == 0:
-                    # result = get_video_list()
-                    # pass
-
-                return render(request, 'workout.html')
-
-
-
-
-
+                #준비운동은 DB에있는거 그대로 던져주고, 본운동은 기존 나이대/등급별 리스트 랜덤던져줌
+                warm_up_list = get_warm_up_video_id(warm_up_db_list, who.age)
+                if warm_up_list == 0:
+                    if who.age == '유소년':
+                        warm_up_list = make_youth_random_warmup(grade)
+                main_list = make_random_main(who.age, grade)
+                return render(request, 'workout.html', {'warm_up_list': warm_up_list, 'main_list': main_list})
 
             else: # 준비운동 있고, 본운동 있고
-                split_warm_up = user_warm_up.split(',')
-                video_id_list=[]
-                for i in range(len(split_warm_up)):
-                    result = get_video_list('국민체력100_운동처방전 '+who.age+" "+split_warm_up[i])
-                    video_id_list.append(result)
-                return render(request, 'workout.html', {'warm_up_list': video_id_list})
+                warm_up_list = get_warm_up_video_id(warm_up_db_list, who.age)
+                if warm_up_list == 0:
+                    if who.age == '유소년':
+                        warm_up_list = make_youth_random_warmup(grade)
+                main_list = get_main_video_id(main_db_list, who.age)
+                if main_list == 0:
+                    if who.age == '유소년':
+                        make_random_main(who.age, grade)
 
-            # picked_exercise= user_match_exercise_list[50] #여러개 있는 친구
-            # picked_exercise1 = user_match_exercise_list[0] #한개만 있는 친구
-            # picked_zero = user_match_exercise_list[300] #준비운동 없는 친구
-            # print(picked_zero.get_warm_up(), '갯수:',len(picked_zero.get_warm_up())) # 없는건 len 0으로 나옴
+                print(warm_up_list,main_list)
 
-
-
-            # print(picked_exercise1.get_warm_up().split(',')) #,으로 나눠서
-            # print('len으로세기:',len(picked_exercise1.get_warm_up().split(','))) #len으로세면 걷기만있을때 1개로나옴
-
-            # picked_exercise.get_warm_up().split(',')
-            # print(picked_exercise, 'id값은:', picked_exercise.get_warm_up())
-            # print(picked_exercise.get_warm_up().split(','))
-            # exercise = Exercise.objects.get(id=1)
-            # exercise.warm_up = exercise.get_warm_up()
-            # split_warm_up = exercise.warm_up.split(',')
-
-
-            #없으면 len()처리했을때 0임 준비운동없을때 보여줄 동영상 주소 하나 던져주자
-
-            # print(exercise.warm_up, '길이는:',len(exercise.warm_up))
-            # print(split_warm_up, '길이는:', len(split_warm_up))
-            return render(request, 'workout.html')
+                return render(request, 'workout.html', {'warm_up_list': warm_up_list,'main_list': main_list})
 
 #측정후에도 다시하고싶을수도 있으니까 나이대별 측정페이지 보여줄수 있음
 def age_different_show_page(request):
