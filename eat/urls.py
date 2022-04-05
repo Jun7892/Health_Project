c77e19b08ae59b7cd4d6bdda50459709d83dc867
gaeul <e0540@hanmail.net>
2022-04-05 12:13:17 +0900

    eat페이지 login_required추가, item관련 주석처리

--- eat/urls.py
+++ eat/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('detail/<int:id>/', views.eat_detail, name='eat_detail'),
     path('detail/<int:id>/bookmark/', views.bookmark, name='bookmark'),
     path('search/', views.eat_search, name='eat_search'),
-    path('item/', views.item_view, name='item'),
+    # path('item/', views.item_view, name='item'),
 ]
